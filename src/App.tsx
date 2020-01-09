364ffcda7be86f74d1e382aa22567b2227510649
un1tT <unit2wall@gmail.com>
2020-01-09 07:02:27 +0700

    notifications layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import axios from 'axios';
-import { Button, Icon, Input } from 'antd';
+import {Button, Icon, Input, message, Statistic} from 'antd';
 import 'antd/dist/antd.css';
 
 import './App.css';
@@ -9,6 +9,14 @@ import {apiUrl} from "./config";
 const App: React.FC = () => {
   const [newChannel, setNewChannel] = useState('');
   const [deleteChannel, setDeleteChannel] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${apiUrl}/channels`).then(({ data }) => {
+      setAmount(data.length);
+    });
+  }, []);
+
   const onChangeNewChannel = ({ target }: ChangeEvent<HTMLInputElement>) => {
     setNewChannel(target.value)
   };
@@ -16,16 +24,39 @@ const App: React.FC = () => {
     setDeleteChannel(target.value)
   };
   const onSubmit = () => {
-    axios.post(`${apiUrl}/channels`, { name: newChannel });
+    axios.post(`${apiUrl}/channels`, { name: newChannel })
+      .then(() => {
+        showSuccessMessage(newChannel, 'принят в легион!')
+        setNewChannel('');
+      })
+      .catch(() => {
+        showErrorMessage();
+      })
+  };
+
+  const showSuccessMessage = (name: string, text: string) => {
+    message.success(`${name} ${text}`);
+  };
+
+  const showErrorMessage = () => {
+    message.error('Цезари тоже ошибаются, что-то пошло не так...');
   };
 
   const onDelete =  () => {
-    axios.delete(`${apiUrl}/channels/${deleteChannel}`);
+    axios.delete(`${apiUrl}/channels/${deleteChannel}`)
+      .then(() => {
+        showSuccessMessage(deleteChannel, 'разжалован!');
+        setDeleteChannel('');
+      })
+      .catch(() => {
+        showErrorMessage();
+      })
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        <Statistic title="Численность легиона" value={amount} prefix={<Icon type="trophy" />} />
         <img src="https://st4.depositphotos.com/1415902/31584/v/450/depositphotos_315845546-stock-illustration-julius-caesar-roman-politician-and.jpg" className="App-logo" alt="logo" />
         <p>
           Добавь свой канал, Тиберий подключится:
@@ -40,7 +71,7 @@ const App: React.FC = () => {
             onPressEnter={onSubmit}
           />
           <Button onClick={onSubmit} className="Submit-button" type="primary">
-            Старт
+            Вступить
             <Icon type="right" />
           </Button>
         </div>
